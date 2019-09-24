277f4d1dd0ac8314061e00cb535811c92f6b306c
Daniel Bisett <daniel@austintatiousdesign.co>
2019-09-24 08:30:58 -0500

    home page mobile responsive

--- template-parts/content/block1.php
+++ template-parts/content/block1.php
@@ -6,54 +6,22 @@
  */
 
 namespace WP_Rig\WP_Rig;
+$block1_title	= get_field('block1_title');
+$block1_content	= get_field('block1_content');
+$block1_phone_link	= get_field('block1_phone_link');
+$block1_phone_txt	= get_field('block1_phone_txt');
 
 ?>
 
 <div class="block1">
-  <!-- <div id="topImg">
-  <amp-img 
-  src="../wp-content/themes/wprig-supremehardwoodfloorsinc/assets/images/floor-sample-min.jpg"
-  width="1536"
-			height="768"
-			layout="responsive"
-      alt="floor-sample-min"
-      media="(max-width: 63.99em)"
-      >
-    </amp-img>
-    <amp-img 
-  src="../wp-content/themes/wprig-supremehardwoodfloorsinc/assets/images/floor-sample-square.jpg"
-  width="6"
-			height="6"
-			layout="responsive"
-      alt="floor-sample-square"
-      media="(min-width: 64em)"
-      >
-		</amp-img>
-</div> -->
+ 
 <div id="contentBlock">
 	<h3>
-  Personalized Service, Fixed Right The First Time
+  <?php echo $block1_title; ?>
 	</h3>
 	<p>
-	Nestled in the heart of the Texas Hill Country, located in the Wimberley Valley you’ll find Road And Track Specialists, your experts in Wimberley motorcycle repair and San Marcos motorcycle repair.  Road And Track Specialists is the Hill Country’s premier motorcycle, ATV/UTV and scooter repair facility.  Owner and Master Technician E. J. Josephson has logged over 40 years of repair, restoration and troubleshooting experience.  Not only was E. J. a line technician at a number of multi-line motorcycle dealerships he spent 23 years with the American Honda Motor Co., Inc in Torrance, California in Motorcycle and Automobile Engineering as well as in the Honda and Acura Customer Service Departments. This experience allows E. J. and Road And Track Specialists to provide factory/manufacturer level technical repair assistance and world-class customer service on a personalized basis to our customers.  As a full service repair facility Road And Track Specialists is the place for mounting and balancing tires, oil change service, scheduled maintenance, fork seal replacement and carburetor overhauls as well as accessory installation like custom exhausts, carburetor jetting kits, LED/HID lighting, custom wiring, braided brake hoses and so much more.  If you’re interested in building a custom Supermoto bike, outfitting an Adventure motorcycle for the ride of a lifetime, restoring your favorite classic, building a retro café racer, setting up your ATV/UTV for that trophy hunt then trust the experts at Road And Track Specialists to make your dreams a reality.  Weather it’s a cruiser, touring, adventure or sport bike, ATV/UTV or scooter Road And Track Specialists is your place for Wimberley motorcycle repair and San Marcos motorcycle repair.  Remember, there is a huge difference between a “factory trained” dealer technician and a technician that worked for the factory.  Bring your favorite machine to Road And Track Specialists and experience the difference for yourself!
+	<?php echo $block1_content; ?>
 	</p>
-	<!-- <amp-lightbox id="my-lightbox" layout="nodisplay">
-    <div class="lightbox" on="tap:my-lightbox.close" role="button" tabindex="0">
-      
-      <amp-iframe width="350" height="300" layout="fixed" 
-              sandbox="allow-scripts allow-same-origin allow-popups" frameborder="0" 
-              src="https://page.co/qQY60">
-    <amp-img layout="fill"
-             src="../wp-content/themes/supremehardwoodfloorsinc/assets/images/supreme-hardwood-floors-form-loading.png"
-             placeholder></amp-img>
-  </amp-iframe>
-    </div>
-  </amp-lightbox> -->
-  <button id="estimate" class="btn btn-lg btn-danger" on="tap:my-lightbox" role="button" tabindex="0"><a href="tel:3102134090">Call for Service Now! »</a></button>
-	<!-- <div class="ctaButton ">
-		<h5>
-			<a href="#">Get A Free Consultation</a>
-		</h5>
-</div> -->
+  <button id="estimate" class="btn btn-lg btn-danger" on="tap:my-lightbox" role="button" tabindex="0"><a href="tel:<?php echo $block1_phone_link; ?>"><?php echo $block1_phone_txt; ?></a></button>
 </div>
 </div>
