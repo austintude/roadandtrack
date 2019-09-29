f4c5db1bbaa2025a3687ffd5e3a56adda7412bd1
Daniel Bisett <daniel@austintatiousdesign.co>
2019-09-28 23:54:13 -0500

    contact page layout and styles

---
+++ template-parts/content/contact.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * Template part for contact
+ *
+ * @package wp_rig
+ */
+
+namespace WP_Rig\WP_Rig;
+$contact_form				= get_field('contact_form');
+$contact_amp_iframe_placeholder				= get_field('contact_amp_iframe_placeholder');
+
+?>
+
+<div id="ctaBlock">
+	<div class="contactForm">
+		<a href="tel:3102134090" rel="noreferrer noopenner"><h3>Call Us Today - (310) 213-4090</h3></a>
+			<h3>Contact Us</h3>
+			<amp-iframe 
+				width="360" 
+				height="360" 
+				layout="fixed" 
+				sandbox="allow-scripts allow-same-origin allow-popups" 
+				frameborder="0" 
+				src="<?php echo $contact_form; ?>"
+				media="(max-width: 43.5em) and (min-height:27em)"
+				>
+				<amp-img layout="fill"
+					src="<?php echo $contact_amp_iframe_placeholder['url']; ?>"
+					placeholder>
+				</amp-img>
+			</amp-iframe>
+			<amp-iframe 
+				width="580" 
+				height="310" 
+				layout="fixed" 
+				sandbox="allow-scripts allow-same-origin allow-popups" 
+				frameborder="0" 
+				src="<?php echo $contact_form; ?>"
+				media="(max-width: 43.5em) and (max-height:26em)"
+				>
+				<amp-img layout="fill"
+					src="<?php echo $contact_amp_iframe_placeholder['url']; ?>"
+					placeholder>
+				</amp-img>
+			</amp-iframe>
+			<amp-iframe 
+				width="610" 
+				height="310" 
+				layout="fixed" 
+				sandbox="allow-scripts allow-same-origin allow-popups" 
+				frameborder="0" 
+				src="<?php echo $contact_form; ?>"
+				media="(min-width: 43.6em) and (min-height:27em) and (max-width: 48.5em)"
+				>
+				<amp-img layout="fill"
+					src="<?php echo $contact_amp_iframe_placeholder['url']; ?>"
+					placeholder>
+				</amp-img>
+			</amp-iframe>
+			<amp-iframe 
+				width="610" 
+				height="350" 
+				layout="fixed" 
+				sandbox="allow-scripts allow-same-origin allow-popups" 
+				frameborder="0" 
+				src="<?php echo $contact_form; ?>"
+				media="(min-width: 48.6em)"
+				>
+				<amp-img layout="fill"
+					src="<?php echo $contact_amp_iframe_placeholder['url']; ?>"
+					placeholder>
+				</amp-img>
+			</amp-iframe>
+  
+	</div>
+</div>
+
